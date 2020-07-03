34d2daef18eba88b8341b089e61d2c654ec86f0e
MuJtaba Basheer <mujtaba.fleapo@gmail.com>
2020-07-03 17:01:07 +0530

    Homepage setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import "./App.css";
+import "./assets/css/style.css";
+import "./assets/css/bootstrap.css";
+import "./assets/css/opus-style.css";
+
+import Opus from "./components/Opus";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Opus path="/" />
+        </Router>
+    );
 }
 
 export default App;
