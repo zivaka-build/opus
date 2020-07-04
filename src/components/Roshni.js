7c20b09b8a20b1eed32537ca36e628e350848185
MuJtaba Basheer <mujtaba.fleapo@gmail.com>
2020-07-04 17:13:08 +0530

    --amend

--- src/components/Roshni.js
+++ src/components/Roshni.js
@@ -16,39 +16,6 @@ const Roshni = () => {
             {/* NavBar */}
             <Nav />
 
-            {/* SHOWCASE */}
-            <section id="showcase" className="pt-5">
-                <div className="primary-overlay text-white">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-6 text-center">
-                                <h1 className="display-2 mt-5 pt-5">
-                                    Do What You Dream Of...
-                                </h1>
-                                <p className="lead">
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Nam, tempora!
-                                </p>
-                                <a
-                                    href="#"
-                                    className="btn btn-outline-secondary btn-lg text-white"
-                                >
-                                    <i className="fas fa-arrow-right"></i> Read
-                                    More
-                                </a>
-                            </div>
-                            <div className="col-lg-6">
-                                <img
-                                    src={require("./../assets/img/book.png")}
-                                    alt=""
-                                    className="img-fluid d-none d-lg-block"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
             {/* ABOUT SECTION */}
             <OurStory />
 
