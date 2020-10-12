859f6a6b81e4899e73a5085118bfd5483bb41cae
Subhradeep <dassubhra1998@gmail.com>
2020-10-12 15:32:45 +0530

    New Banner Added

--- src/components/Aashakiran.js
+++ src/components/Aashakiran.js
@@ -26,7 +26,7 @@ const Ashakiran = () => {
       <section id="showcase" className="pt-3 pb-3 text-center bg-light">
       
          
-      <img id="banner" className="col-12 col-sm-12 col-md-12 col-lg-12" src={require("./../assets/img/AashaKiran-Banner-1.jpg")} />
+      <img id="banner" className="col-12 col-sm-12 col-md-12 col-lg-12" src={require("./../assets/img/AashaKiran-Banner-2.jpg")} />
    
       </section>
 
